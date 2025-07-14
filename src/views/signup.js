d2c445e052e93714eb0db295d1efbe6a2a543ac6
CarolinaDeLeón <carolinadeleonperez@gmail.com>
2025-07-14 14:01:01 +0200

    Sign Up form with apiUrl

--- src/views/signup.js
+++ src/views/signup.js
@@ -1,3 +1,5 @@
+import { createNewUser } from "../api/apiUsers";
+import { navigate } from "../router";
 /*<html lang="es-ES">
     <head>
         <meta charset="UTF-8">
@@ -20,21 +22,21 @@ document.addEventListener('DOMContentLoaded', () => {
     app.innerHTML = `
         <h2>Registro</h2>
         <form id="signupForm">
-            <label for="name">Nombre:</label>
-            <input type="text" id="name" required>
-            <label for="password">Contraseña:</label>
-            <input type="password" id="password" required>
-            <label for="confirmPassword">Repite la contraseña:</label>
-            <input type="password" id="confirmPassword" required>
-            <button type="submit">Regístrate</button>
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" required>
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" required>
+        <label for="confirmPassword">Repite la contraseña:</label>
+        <input type="password" id="confirmPassword" required>
+        <button type="submit">Regístrate</button>
         </form>
         <p>¿Ya tienes una cuenta? <a href="login.js">Inicia sesión aquí</a></p>
     `;
 
     //FUNCIONALIDAD FORMULARIO
-    const form = document.getElementById('signupForm');
+    const signupform = document.getElementById('signup-form');
 
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', async (event) => {
         event.preventDefault();
 
         const name = document.getElementById('name').value.trim();
@@ -46,9 +48,30 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        console.log('Nombre:', name);
-        console.log('Registro exitoso');
-        //UTILIZAR PARA ENVIAR LOS DATOS A UN SERVIDOR CON FETCH()
+        //MOCKAPI TEMPORAL 
+        const apiUrl = 'https://6874d617dd06792b9c95731e.mockapi.io/users'
 
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: name,
+                    password: password
+                })
+            });
+            
+            if (!response.ok) throw new Error('Se ha producido un error durante el registro');
+
+            const data = await response.json();
+            console.log('Usuario registrado:', data);
+            alert('¡Te has registrado!');
+            form.reset()
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Ocurrió un error al registrarse');
+        }
     });
 });
