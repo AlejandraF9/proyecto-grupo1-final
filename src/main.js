35789afaadd33475b45fffdb4cbd68d3e3d9a2bf
Sintra <sintradacosta@icloud.com>
2025-07-20 22:31:28 +0100

    The components folder is created and the navbar is moved to main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
 import { userLogin } from "../src/views/login";
-
-userLogin();
-
 import { generatePaymentForm } from "./api/apiPayment";
-
-const container = document.getElementById("app");
-generatePaymentForm(container);
-
 import { renderSignupView } from './views/signup.js';
+import { renderNavbar } from "./components/navbar.js";
+import { renderFooter } from "./components/footer.js";
 
 document.addEventListener('DOMContentLoaded', () => {
+  userLogin();
   renderSignupView(); // Esta función debe encargarse de montar la vista de registro
+
+  const container = document.getElementById("app");
+generatePaymentForm(container);
+
+
+//UI navbar y footer
+  renderNavbar();
+  console.log("✅ Navbar DOM insertado completamente");
+  renderFooter();
 });
+
+
+
+
+
+
+
