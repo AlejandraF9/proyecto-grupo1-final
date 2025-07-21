03c43d16b068e9e6d2ca5c32eb2d9780f22a6631
CarolinaDeLeón <carolinadeleonperez@gmail.com>
2025-07-21 19:48:39 +0200

    Bio view created and modificated with createElement(), funtion renderBio, functionCreateParagraph, function createTextDiv, function createSectionQuienesSomos, function createSectionQueTenemos, function createBioView

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { renderHero } from "./views/hero.js";
 import { renderCategorys } from "./views/categorys.js";
 import { generatePaymentForm } from "./api/apiPayment.js";
 import { renderShop } from "./views/shop.js";
+import { renderBio } from "./views/bio.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   //const usuario = JSON.parse(localStorage.getItem("current-user"));
@@ -15,4 +16,9 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const container = document.getElementById("app");
   generatePaymentForm(container);
+
+  //Render Bio
+  const bio = renderBio();
+  container.appendChild(bio);
 });
+
