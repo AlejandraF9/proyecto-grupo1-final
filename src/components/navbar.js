bc66bde9b63008d1cf808dbb56e979bd9b3dea6f
CarolinaDeLeón <carolinadeleonperez@gmail.com>
2025-07-31 11:14:46 +0200

    First navbar icons changed to svg and css hero modificated

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -70,11 +70,11 @@ export function renderNavbar() {
   navbarContainer.appendChild(navbarContainerA);
 
   //creo el enlace de instagram
-  const instaNavbar = document.createElement("img");
-  instaNavbar.className = "rrss-icons";
-  instaNavbar.src = insta_icon;
-  instaNavbar.alt = "logo de instagram";
-  navbarContainerA.appendChild(instaNavbar);
+  //const instaNavbar = document.createElement("img");
+  //instaNavbar.className = "rrss-icons";
+  //instaNavbar.src = insta_icon;
+  //instaNavbar.alt = "logo de instagram";
+  //navbarContainerA.appendChild(instaNavbar);
 
   //creo el enlace de bluesky
   /* const blueskykaNavbar = document.createElement("img");
@@ -82,6 +82,13 @@ export function renderNavbar() {
   blueskykaNavbar.src = bluesky_icon;
   blueskykaNavbar.alt = "logo de Bluesky";
   navbarContainerA.appendChild(blueskykaNavbar);*/
+
+  //creo el enlace de youTube
+  //const youtubeNavbar = document.createElement("img");
+  //youtubeNavbar.className = "rrss-icons";
+  //youtubeNavbar.src = youtube_icon;
+  //youtubeNavbar.alt = "logo de youTube";
+  //navbarContainerA.appendChild(youtubeNavbar);
 
   const facebookLinkNavBar = document.createElement("a");
   facebookLinkNavBar.href = "#";
@@ -97,12 +104,51 @@ export function renderNavbar() {
 </svg>`;
   navbarContainerA.appendChild(facebookLinkNavBar);
 
-  //creo el enlace de youTube
-  const youtubeNavbar = document.createElement("img");
-  youtubeNavbar.className = "rrss-icons";
-  youtubeNavbar.src = youtube_icon;
-  youtubeNavbar.alt = "logo de youTube";
-  navbarContainerA.appendChild(youtubeNavbar);
+  const instagramLinkNavBar = document.createElement("a");
+  instagramLinkNavBar.href = "#";
+  instagramLinkNavBar.rel = "noopener noreferrer";
+  instagramLinkNavBar.className = "footer-rrss";
+  instagramLinkNavBar.innerHTML = `<svg width="30" xmlns="http://www.w3.org/2000/svg" height="30" id="screenshot-1a2ad72a-1d06-8080-8006-80379f5371e2" viewBox="698 2618 24 24" style="-webkit-print-color-adjust::exact" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1">
+  <g id="shape-1a2ad72a-1d06-8080-8006-80379f5371e2" style="fill:#000000" width="24" height="24" rx="0" ry="0">
+    <g id="shape-1a2ad72a-1d06-8080-8006-80379f5371e6" style="display:none">
+      <g class="fills" id="fills-1a2ad72a-1d06-8080-8006-80379f5371e6">
+        <rect rx="0" ry="0" x="698" y="2618" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" width="24" height="24" fill="none" style="fill:none">
+        </rect>
+      </g>
+    </g>
+    <g id="shape-1a2ad72a-1d06-8080-8006-80379f5371e7">
+      <g class="fills" id="fills-1a2ad72a-1d06-8080-8006-80379f5371e7">
+        <path d="M705.800048828125,2620L714.199951171875,2620C717.4000244140625,2620,720,2622.60009765625,720,2625.800048828125L720,2634.199951171875C720,2635.73828125,719.388916015625,2637.21337890625,718.30126953125,2638.30126953125C717.2135009765625,2639.388916015625,715.73828125,2640,714.199951171875,2640L705.800048828125,2640C702.5999755859375,2640,700,2637.39990234375,700,2634.199951171875L700,2625.800048828125C700,2622.5966796875,702.5966796875,2620,705.800048828125,2620M705.5999755859375,2622C703.61181640625,2622,702,2623.61181640625,702,2625.60009765625L702,2634.39990234375C702,2636.39013671875,703.6099853515625,2638,705.5999755859375,2638L714.4000244140625,2638C715.354736328125,2638,716.2705078125,2637.62060546875,716.945556640625,2636.945556640625C717.6207275390625,2636.2705078125,718,2635.354736328125,718,2634.39990234375L718,2625.60009765625C718,2623.60986328125,716.3900146484375,2622,714.4000244140625,2622ZM715.25,2623.5C715.9404296875,2623.5,716.5,2624.0595703125,716.5,2624.75C716.5,2625.4404296875,715.9404296875,2626,715.25,2626C714.5595703125,2626,714,2625.4404296875,714,2624.75C714,2624.0595703125,714.5595703125,2623.5,715.25,2623.5M710,2625C712.761474609375,2625,715,2627.238525390625,715,2630C715,2632.761474609375,712.761474609375,2635,710,2635C707.238525390625,2635,705,2632.761474609375,705,2630C705,2627.238525390625,707.238525390625,2625,710,2625M710,2627C708.3431396484375,2627,707,2628.34326171875,707,2630C707,2631.65673828125,708.3431396484375,2633,710,2633C711.6568603515625,2633,713,2631.65673828125,713,2630C713,2628.34326171875,711.6568603515625,2627,710,2627" style="fill:#ffffff">
+        </path>
+      </g>
+    </g>
+  </g>
+</svg>`;
+  navbarContainerA.appendChild(instagramLinkNavBar);
+
+  const blueskyLinkNavBar = document.createElement("a");
+  blueskyLinkNavBar.href = "#";
+  blueskyLinkNavBar.rel = "noopener noreferrer";
+  blueskyLinkNavBar.className = "footer-rrss";
+  blueskyLinkNavBar.innerHTML = `<svg width="30" xmlns="http://www.w3.org/2000/svg" height="30" id="screenshot-1a2ad72a-1d06-8080-8006-80379f5371e1" viewBox="738 2618 24 24" style="-webkit-print-color-adjust::exact" xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" version="1.1">
+  <g id="shape-1a2ad72a-1d06-8080-8006-80379f5371e1" style="fill:#000000" width="24" height="24" rx="0" ry="0">
+    <g id="shape-1a2ad72a-1d06-8080-8006-80379f5371e4" style="display:none">
+      <g class="fills" id="fills-1a2ad72a-1d06-8080-8006-80379f5371e4">
+        <rect rx="0" ry="0" x="738" y="2618" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" width="24" height="24" fill="none" style="fill:none">
+        </rect>
+      </g>
+    </g>
+    <g id="shape-1a2ad72a-1d06-8080-8006-80379f5371e5">
+      <g class="fills" id="fills-1a2ad72a-1d06-8080-8006-80379f5371e5">
+        <path d="M750,2629.387939453125C749.093994140625,2627.626953125,746.6279296875,2624.343994140625,744.3349609375,2622.72607421875C742.137939453125,2621.176025390625,741.301025390625,2621.44287109375,740.751953125,2621.69287109375C740.115966796875,2621.97802734375,740,2622.955078125,740,2623.52783203125C740,2624.10302734375,740.31494140625,2628.237060546875,740.52001953125,2628.927978515625C741.199951171875,2631.2080078125,743.614013671875,2631.97802734375,745.840087890625,2631.73095703125C742.580078125,2632.2138671875,739.6829833984375,2633.40087890625,743.47998046875,2637.62890625C747.657958984375,2641.9541015625,749.2060546875,2636.7021484375,750,2634.0390625C750.7939453125,2636.7021484375,751.7080078125,2641.76513671875,756.4439697265625,2637.62890625C760,2634.0390625,757.4210205078125,2632.2138671875,754.1610107421875,2631.73095703125C756.385986328125,2631.97802734375,758.801025390625,2631.2080078125,759.47998046875,2628.927978515625C759.68505859375,2628.238037109375,760,2624.10302734375,760,2623.529052734375C760,2622.9541015625,759.884033203125,2621.97900390625,759.248046875,2621.69091796875C758.698974609375,2621.44287109375,757.862060546875,2621.173828125,755.6650390625,2622.72412109375C753.3720703125,2624.344970703125,750.905029296875,2627.6279296875,750,2629.387939453125" style="fill:#ffffff">
+        </path>
+      </g>
+    </g>
+  </g>
+</svg>`;
+  navbarContainerA.appendChild(blueskyLinkNavBar);
+
+
 
   //creo el segundo contenedor que subdivide el navbar
 
